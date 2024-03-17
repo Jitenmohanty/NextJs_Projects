6f3ffb11870236286795975aeff36b45a75963fd
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2024-03-17 19:31:55 +0530

    Some optimization done

--- nextjs_dashboard/src/app/ui/post/page.tsx
+++ nextjs_dashboard/src/app/ui/post/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import ImageGallery from "@/components/Image";
 import Post from "@/components/Post";
 import {
@@ -23,28 +21,20 @@ const Posts = () => {
   const [photos, setPhotos] = useState(photosRes);
   const [posts, setPosts] = useState(postsRes);
   const [page, setPage] = useState(1);
-  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     setPhotos(photosRes);
     setPosts(postsRes);
-    setCurrentIndex(0);
-  }, [photos, posts, photosRes, postsRes]);
+  }, [photosRes, postsRes]);
 
   const totalPages = Math.ceil(posts?.length / 20);
 
   const nextPage = () => {
     setPage((prevPage) => prevPage + 1);
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % photos?.length);
-    setCurrentIndex(0);
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + photos?.length) % photos?.length
-    );
   };
 
   const prevPage = () => {
     setPage((prevPage) => prevPage - 1);
-    setCurrentIndex(0);
   };
 
   const handleLike = (post: any, photos: any) => {
@@ -54,9 +44,11 @@ const Posts = () => {
     };
     dispatch(likePost(posts));
   };
+
   const handleUnLike = (id: any) => {
     dispatch(unlikePost(id));
   };
+
   const handleSavepost = (post: any, photos: any) => {
     const posts = {
       post,
@@ -64,6 +56,7 @@ const Posts = () => {
     };
     dispatch(savePost(posts));
   };
+
   const handleUnSavepost = (id: any) => {
     dispatch(unsavePost(id));
   };
@@ -79,8 +72,8 @@ const Posts = () => {
   return (
     <div className="w-full flex flex-col gap-4">
       <div className=" flex flex-col gap-4">
-        {posts &&
-          posts.slice(page * 20 - 20, page * 20).map((post: any, index) => (
+        {posts.length > 0 &&
+          posts.slice(page * 20 - 20, page * 20).map((post: any, index: number) => (
             <div
               key={index}
               className="bg-blue-950 min-h-[45vh]  lg:min-h-[60vh] flex flex-col border-[.1px] rounded-lg p-3 text-white border-yellow-100 "
@@ -91,50 +84,49 @@ const Posts = () => {
               <Post post={post} />
               <div className="flex justify-end items-center gap-2">
                 {likedPosts &&
-                likedPosts.some((like: any) => like.post.id === post.id) ? (
-                  <button
-                    onClick={() => handleUnLike(post.id)}
-                    className="text-3xl text-green-400"
-                  >
-                    <AiFillLike />
-                  </button>
-                ) : (
-                  <button
-                    onClick={() =>
-                      handleLike(post, photos[(page - 1) * 20 + index])
-                    }
-                    className="text-3xl"
-                  >
-                    <BiLike />
-                  </button>
-                )}
+                  likedPosts.some((like: any) => like.post.id === post.id) ? (
+                    <button
+                      onClick={() => handleUnLike(post.id)}
+                      className="text-3xl text-green-400"
+                    >
+                      <AiFillLike />
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() =>
+                        handleLike(post, photos[(page - 1) * 20 + index])
+                      }
+                      className="text-3xl"
+                    >
+                      <BiLike />
+                    </button>
+                  )}
                 {savedPosts &&
-                savedPosts.some((save: any) => save.post.id === post.id) ? (
-                  <button
-                    onClick={() => handleUnSavepost(post.id)}
-                    className="text-2xl text-green-400"
-                  >
-                    <FaSave />
-                  </button>
-                ) : (
-                  <button
-                    onClick={() =>
-                      handleSavepost(post, photos[(page - 1) * 20 + index])
-                    }
-                    className="text-2xl"
-                  >
-                    <FaRegSave />
-                  </button>
-                )}
+                  savedPosts.some((save: any) => save.post.id === post.id) ? (
+                    <button
+                      onClick={() => handleUnSavepost(post.id)}
+                      className="text-2xl text-green-400"
+                    >
+                      <FaSave />
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() =>
+                        handleSavepost(post, photos[(page - 1) * 20 + index])
+                      }
+                      className="text-2xl"
+                    >
+                      <FaRegSave />
+                    </button>
+                  )}
               </div>
             </div>
           ))}
       </div>
       <div className="flex justify-between items-center">
-        {" "}
         <button
           disabled={page === 1}
-          className={` ${
+          className={`${
             page === 1 ? "opacity-65" : "opacity-100"
           } px-2 py-1 bg-orange-400 font-semibold rounded-md `}
           onClick={prevPage}
@@ -143,7 +135,7 @@ const Posts = () => {
         </button>
         <button
           disabled={page === totalPages}
-          className={` ${
+          className={`${
             page === totalPages ? "opacity-65" : "opacity-100"
           } px-2 py-1 bg-orange-400 font-semibold rounded-md`}
           onClick={nextPage}
