bd91fd3c32ac9a1bf4fbedecfe91a9c08dd66af7
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2024-03-28 22:22:30 +0530

    LiveBlocks chat functionality done

--- next_js_figma/components/Live.tsx
+++ next_js_figma/components/Live.tsx
@@ -1,10 +1,46 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useMyPresence, useOthers } from "../liveblocks.config";
 import LiveCursor from "./cursor/LiveCursor";
+import { CursorMode, CursorState } from "../types/type";
+import CursorChart from "./cursor/CursorChart";
 
 const Live = () => {
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+  const [cursorState, setCursorState] = useState<CursorState>({
+    mode: CursorMode.Hidden,
+  });
+
+  useEffect(() => {
+    const onKeyUp = (e: KeyboardEvent) => {
+      if (e.key === "/") {
+        setCursorState({
+          mode: CursorMode.Chat,
+          previousMessage: null,
+          message: "",
+        });
+      } else if (e.key === "Escape") {
+        updateMyPresence({ message: "" });
+        setCursorState({
+          mode: CursorMode.Hidden,
+        });
+      }
+    };
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "/") {
+        e.preventDefault();
+      }
+    };
+
+    window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      window.removeEventListener("keyup", onKeyUp);
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [updateMyPresence]);
 
   const handlePonterMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault();
@@ -13,8 +49,9 @@ const Live = () => {
 
     updateMyPresence({ cursor: { x, y } });
   }, []);
+
   const handlePonterLeave = useCallback((event: React.PointerEvent) => {
-    event.preventDefault();
+    setCursorState({ mode: CursorMode.Hidden });
 
     updateMyPresence({ cursor: null, message: null });
   }, []);
@@ -34,6 +71,14 @@ const Live = () => {
       className="h-[100vh] w-full flex justify-center items-center"
     >
       <LiveCursor others={others} />
+      {cursor && (
+        <CursorChart
+          cursor={cursor}
+          cursorState={cursorState}
+          setCursorState={setCursorState}
+          updateMyPresence={updateMyPresence}
+        />
+      )}
       <h1 className="text-5xl text-white">Liveblocks Figma clone</h1>
     </div>
   );
--- next_js_figma/components/cursor/Cursor.tsx
+++ next_js_figma/components/cursor/Cursor.tsx
@@ -15,7 +15,17 @@ const Cursor = ({ colors, x, y, message }: Props) => {
       style={{ transform: `translateX(${x}px) translateY(${y}px)` }}
     >
         <CursorSVG color={colors}/>
-        
+        {/* Meassage */}
+        {
+          message && <div
+          className={`absolute left-2 top-5 rounded-md px-4 py-2`}
+          style={{ backgroundColor: colors }}
+        >
+          <p className='whitespace-nowrap text-lg leading-relaxed text-white'>
+            {message}
+          </p>
+        </div>
+        }
     </div>
   );
 };
