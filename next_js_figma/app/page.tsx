e318888c906ce6db89222e8dc757882a3ebf31bf
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2024-04-06 19:39:15 +0530

    Design Tools done.....

--- next_js_figma/app/page.tsx
+++ next_js_figma/app/page.tsx
@@ -4,16 +4,25 @@ import { useEffect, useRef, useState } from "react";
 import Live from "../components/Live";
 import Navbar from "../components/Navbar";
 import { fabric } from "fabric";
-import { handleCanvasMouseDown, handleCanvasMouseUp, handleCanvasObjectModified, handleCanvaseMouseMove, handleResize, initializeFabric, renderCanvas } from "@/lib/canvas";
+import {
+  handleCanvasMouseDown,
+  handleCanvasMouseUp,
+  handleCanvasObjectModified,
+  handleCanvasSelectionCreated,
+  handleCanvaseMouseMove,
+  handleResize,
+  initializeFabric,
+  renderCanvas,
+} from "@/lib/canvas";
 import LeftSidebar from "@/components/LeftSidebar";
 import RightSidebar from "@/components/RightSidebar";
-import { ActiveElement } from "@/types/type";
+import { ActiveElement, Attributes } from "@/types/type";
 import { useMutation, useRedo, useStorage, useUndo } from "@/liveblocks.config";
 import { defaultNavElement } from "@/constants";
 import { handleDelete, handleKeyDown } from "@/lib/key-events";
+import { handleImageUpload } from "@/lib/shapes";
 
 export default function Page() {
-
   const undo = useUndo();
   const redo = useRedo();
 
@@ -23,70 +32,82 @@ export default function Page() {
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>(null);
   const activeObjectRef = useRef<fabric.Object | null>(null);
-  const imageInputRef = useRef<HTMLInputElement>(null)
+  const imageInputRef = useRef<HTMLInputElement>(null);
+  const isEditingRef = useRef(false);
 
-  const canvasObjects = useStorage((root)=>root.canvasObjects);
+  const [elementAttributes, setElementAttributes] = useState<Attributes>({
+    width: "",
+    height: "",
+    fontSize: "",
+    fontFamily: "",
+    fontWeight: "",
+    fill: "#aabbcc",
+    stroke: "#aabbcc",
+  });
 
+  const canvasObjects = useStorage((root) => root.canvasObjects);
 
+  const syncShapeInStorage = useMutation(({ storage }, object) => {
+    if (!object) return;
 
-  const syncShapeInStorage = useMutation(({storage},object)=>{
-    if(!object) return;
-
-    const {objectId} = object;
+    const { objectId } = object;
     const shapeData = object.toJSON();
     shapeData.objectId = objectId;
 
     const canvasObjects = storage.get("canvasObjects");
 
-    canvasObjects.set(objectId,shapeData);
+    canvasObjects.set(objectId, shapeData);
+  }, []);
 
-  },[])
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: "",
+    value: "",
+    icon: "",
+  });
 
-  const [activeElement,setActiveElement]  = useState<ActiveElement>({
-    name:"",
-    value:"",
-    icon:""
-  })
+  const deleteAllShapes = useMutation(({ storage }) => {
+    const canvasObjects = storage.get("canvasObjects");
 
-  const deleteAllShapes = useMutation(({storage},)=>{
-      const canvasObjects = storage.get('canvasObjects')
+    if (!canvasObjects || canvasObjects.size === 0) return true;
 
-      if(!canvasObjects || canvasObjects.size === 0) return true;
+    for (const [key, value] of canvasObjects.entries()) {
+      canvasObjects.delete(key);
+    }
 
-      for(const [key,value] of canvasObjects.entries()){
-        canvasObjects.delete(key)
-      }
+    return canvasObjects.size === 0;
+  }, []);
+  const deleteShapeFromStorage = useMutation(({ storage }, shapeId) => {
+    const canvasObjects = storage.get("canvasObjects");
 
-      return canvasObjects.size === 0;
+    canvasObjects.delete(shapeId);
+  }, []);
 
-  },[])
-   const deleteShapeFromStorage = useMutation(({storage},shapeId)=>{
-      const canvasObjects = storage.get('canvasObjects');
-
-      canvasObjects.delete(shapeId)
-
-   },[])
-
-  const handleActiveElement = (elem:ActiveElement)=>{
+  const handleActiveElement = (elem: ActiveElement) => {
     setActiveElement(elem);
 
-    switch(elem?.value){
-      case 'reset':
+    switch (elem?.value) {
+      case "reset":
         deleteAllShapes();
         fabricRef.current?.clear();
-        setActiveElement(defaultNavElement)
-        break;
-      case 'delete':
-        handleDelete(fabricRef.current as any,deleteShapeFromStorage);
         setActiveElement(defaultNavElement);
+        break;
+      case "delete":
+        handleDelete(fabricRef.current as any, deleteShapeFromStorage);
+        setActiveElement(defaultNavElement);
+      case "image":
+        imageInputRef.current?.click();
+        isDrawing.current = false;
+        if (fabricRef.current) {
+          fabricRef.current.isDrawingMode = false;
+        }
+        break;
       default:
         break;
     }
 
     selectedShapeRef.current = elem?.value as string;
-  }
+  };
 
-  
   useEffect(() => {
     // initialize the fabric canvas
     const canvas = initializeFabric({
@@ -117,7 +138,7 @@ export default function Page() {
         selectedShapeRef,
         isDrawing,
         shapeRef,
-        syncShapeInStorage
+        syncShapeInStorage,
       });
     });
     canvas.on("mouse:up", () => {
@@ -128,26 +149,31 @@ export default function Page() {
         shapeRef,
         syncShapeInStorage,
         setActiveElement,
-        activeObjectRef
+        activeObjectRef,
       });
     });
 
-    canvas.on("object:modified",(options)=>{
-        handleCanvasObjectModified({
-          options,
-          syncShapeInStorage,
-          
-        })
-    })
-    
+    canvas.on("object:modified", (options) => {
+      handleCanvasObjectModified({
+        options,
+        syncShapeInStorage,
+      });
+    });
 
-    
+    canvas.on("selection:created",(options)=>{
+      handleCanvasSelectionCreated({
+        options,
+        isEditingRef,
+        setElementAttributes
+      })
+    })
+
     /**
      * listen to the resize event on the window which is fired when the
      * user resizes the window.
-    *
-    * We're using this to resize the canvas when the user resizes the
-    * window.
+     *
+     * We're using this to resize the canvas when the user resizes the
+     * window.
      */
     window.addEventListener("resize", () => {
       handleResize({
@@ -155,40 +181,58 @@ export default function Page() {
       });
     });
 
-    window.addEventListener("keydown",(e)=>{
+    window.addEventListener("keydown", (e) => {
       handleKeyDown({
         e,
-        canvas:fabricRef.current,
+        canvas: fabricRef.current,
         undo,
         redo,
         syncShapeInStorage,
         deleteShapeFromStorage,
-      })
-    })
+      });
+    });
 
-    return ()=>{
+    return () => {
       canvas.dispose();
       // canvas.clear();
-    }
-    
+    };
   }, [canvasRef]); // run this effect only once when the component mounts and the canvasRef changes
 
-  useEffect(()=>{
+  useEffect(() => {
     renderCanvas({
       fabricRef,
       canvasObjects,
-      activeObjectRef
-    })
-  },[canvasObjects])
-
+      activeObjectRef,
+    });
+  }, [canvasObjects]);
 
   return (
-    <div className="h-screen overflow-hidden">
-      <Navbar activeElement={activeElement} handleActiveElement={handleActiveElement}/>
-      <section className="flex h-full  flex-row">
-        <LeftSidebar allShapes={Array.from(canvasObjects)}/>
-        <Live canvasRef={canvasRef}/>
-        <RightSidebar/>
+    <div className='h-screen overflow-hidden'>
+      <Navbar
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
+        imageInputRef={imageInputRef}
+        handleImageUpload={(e: any) => {
+          e.stopPropagation();
+          handleImageUpload({
+            file: e.target.files[0],
+            canvas: fabricRef as any,
+            shapeRef,
+            syncShapeInStorage,
+          });
+        }}
+      />
+      <section className='flex h-full  flex-row'>
+        <LeftSidebar allShapes={Array.from(canvasObjects)} />
+        <Live canvasRef={canvasRef} />
+        <RightSidebar
+          isEditingRef={isEditingRef}
+          elementAttributes={elementAttributes}
+          setElementAttributes={setElementAttributes}
+          fabricRef={fabricRef}
+          activeObjectRef={activeObjectRef}
+          syncShapeInStorage={syncShapeInStorage}
+        />
       </section>
     </div>
   );
--- next_js_figma/components/RightSidebar.tsx
+++ next_js_figma/components/RightSidebar.tsx
@@ -1,9 +1,46 @@
+import { RightSidebarProps } from "@/types/type";
 import React from "react";
+import Dimensions from "./settings/Dimensions";
+import Color from "./settings/Color";
+import Export from "./settings/Export";
+import Text from "./settings/Text";
+import { modifyShape } from "@/lib/shapes";
 
-const RightSidebar = () => {
+const RightSidebar = ({elementAttributes,setElementAttributes,isEditingRef,fabricRef,activeObjectRef,syncShapeInStorage}:RightSidebarProps) => {
+  
+  const handleInputChange = (property:string,value:string)=>{
+      if(!isEditingRef.current) isEditingRef.current = true;
+      
+      setElementAttributes((prev)=>(
+        {...prev,[property]:value}
+      ))
+
+      modifyShape({
+        canvas:fabricRef.current as fabric.Canvas,
+        property,
+        value,
+        activeObjectRef,
+        syncShapeInStorage
+      })
+  }
+
   return (
     <section className='sticky right-0 flex  min-w-[227px] select-none flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 max-sm:hidden'>
       <h3 className=' px-5 pt-4 text-xs uppercase'>Design</h3>
+      <span className="text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-200 pb-4">
+          Make changes to canvas as you like
+        </span>
+        <Dimensions
+          width={elementAttributes.width}
+          height={elementAttributes.height}
+          handleInputChange={handleInputChange}
+          isEditingRef={isEditingRef}
+          
+        />
+        <Text/>
+        <Color/>
+        <Color/>
+        <Export/>
     </section>
   );
 };
