2a88c0efe0a683bf02a9c9a4c582d6ad3237fea2
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2024-09-09 20:17:57 +0530

    Add some packeges for AI

--- next_js_feedback_system/src/components/Navbar.tsx
+++ next_js_feedback_system/src/components/Navbar.tsx
@@ -1,27 +1,47 @@
-"use client"
+"use client";
 
 import { User } from "next-auth";
 import { signOut, useSession } from "next-auth/react";
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
   const { data: session } = useSession();
   const user: User = session?.user as User;
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      router.replace('/')
+    }
+  }, [session]);
 
   return (
     <nav className="p-4 md:p-4 shadow-md bg-gray-900 text-white">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <a href="#" className="text-xl font-bold mb-4 md:mb-0">
-          True Feedback
-        </a>
+        <div className=" flex gap-4">
+          <a href="#" className="text-xl font-bold mb-4 md:mb-0">
+            True Feedback
+          </a>
+          {session ? (
+            <Link
+              className=" border-[1px] p-[4px] text-sm hover:border-gray-600 transition-all ease-in-out rounded-md border-blue-400"
+              href={"/dashboard"}
+            >
+              Dashboard
+            </Link>
+          ) : (
+            <></>
+          )}
+        </div>
         {session ? (
           <>
             <span className="mr-4">Welcome, {user.username || user.email}</span>
             <Button
               onClick={() => signOut()}
-              className='w-full md:w-auto hover:bg-red-500 font-bold hover:text-white transition-all ease-in-out  bg-slate-100 text-black'
+              className="w-full md:w-auto hover:bg-red-500 font-bold hover:text-white transition-all ease-in-out  bg-slate-100 text-black"
             >
               Logout
             </Button>
