7c872d7db44ec31918085371f3029329a06b9aad
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2025-01-06 09:43:13 +0530

    Resolve some production issue on types error

--- next_js_feedback_system/src/types/ApiResponse.ts
+++ next_js_feedback_system/src/types/ApiResponse.ts
@@ -1,7 +1,7 @@
-import { Message } from "@/model/user.model";
+import { MessageDocument } from "@/model/user.model";
 export interface ApiResponse{
     success:boolean;
     message:string;
     isAccesptingMessage?:boolean;
-    messages?:Array<Message>
+    messages?:Array<MessageDocument>
 }
