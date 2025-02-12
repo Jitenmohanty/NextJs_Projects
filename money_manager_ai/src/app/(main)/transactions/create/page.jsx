ceec9f5cae80d12c483d539bae978ec0848852ad
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2025-02-12 22:36:34 +0530

    ADiing Ai Recipet scanning feature with adding inside transactions

--- money_manager_ai/src/app/(main)/transactions/create/page.jsx
+++ money_manager_ai/src/app/(main)/transactions/create/page.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { AddTransactionForm } from "@/components/transaction-form";
+import { getUserAccount } from "@/api/dashboard";
+import { getTransaction } from "@/api/transaction";
+import { defaultCategories } from "@/data/catagories";
 
-const CreateTransactions = () => {
+export default async function AddTransactionPage({ searchParams }) {
+  const accounts = await getUserAccount();
+  const editId = searchParams?.edit;
+
+  let initialData = null;
+  if (editId) {
+    const transaction = await getTransaction(editId);
+    initialData = transaction;
+  }
+
   return (
-    <div>
-      
+    <div className="max-w-3xl mx-auto px-5">
+      <div className="flex justify-center md:justify-normal mb-8">
+        <h1 className="text-5xl gradient-title ">Add Transaction</h1>
+      </div>
+      <AddTransactionForm
+        accounts={accounts}
+        categories={defaultCategories}
+        editMode={!!editId}
+        initialData={initialData}
+      />
     </div>
-  )
+  );
 }
-
-export default CreateTransactions
